6ce792119fb430e4ed65e43a0fca81ade57c86fd
Ri-Reyan <rireyan50@gmail.com>
2026-05-24 00:21:12 +0600

    fix: add .js ext in dist app

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,20 @@
-import express, {} from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import globalErrorhandler from "./utils/globalError";
-import { authRouter } from "./modules/auth/auth.route";
-import { issuesRouter } from "./modules/issues/issues.route";
-import { config } from "./config/secretEnvs";
+import globalErrorhandler from "./utils/globalError.js";
+import { authRouter } from "./modules/auth/auth.route.js";
+import { issuesRouter } from "./modules/issues/issues.route.js";
+import { config } from "./config/secretEnvs.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
+app.use(
+  cors({
     origin: `http://localhost:${config.port}`,
     credentials: true,
-}));
+  }),
+);
 app.use("/api/auth", authRouter);
 app.use("/api/issues", issuesRouter);
 app.use(globalErrorhandler);
